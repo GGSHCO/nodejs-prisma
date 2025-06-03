bbead318f6c9534d1da2607a482b8fa66a39e8f2
rakesh-syf <rakesh@ggsh.in>
2025-06-03 16:00:58 +0530

    cors link updated for vercel

--- src/middleware/security.ts
+++ src/middleware/security.ts
@@ -12,7 +12,7 @@ import { env } from '../config/env'
 
 const corsOrigin =
   process.env.NODE_ENV === 'prod'
-    ? ['https://www.speedyourfin.ai', 'https://syf-nextjs.vercel.app/']
+    ? ['https://www.speedyourfin.ai', 'https://syf-nextjs.vercel.app']
     : ['http://localhost:3001', 'http://192.168.1.15:3001']
 
 console.log('CORS Origin:', corsOrigin)
