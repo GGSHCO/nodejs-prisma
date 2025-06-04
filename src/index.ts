7d9777f8a73b15a4406e4df2134541924fa1bb10
rakesh-syf <rakesh@ggsh.in>
2025-06-04 16:29:22 +0530

    Bug fix in Cookie setup

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,7 @@ app.get('/api/csrf-token', (req, res) => {
 })
 
 app.get('/health', (req, res) => {
-  res.json({ status: 'ok', timestamp: new Date(), message: '4:22' })
+  res.json({ status: 'ok', timestamp: new Date(), message: '4:29' })
 })
 
 // --- Security Middleware (including CSRF, Cors) ---
