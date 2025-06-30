274cf81bbc3f372a07ce5a2f196c87f59b3e1e82
GGSH CO LLP <speed@ggsh.in>
2025-06-30 16:41:08 +0530

    syftestenv.speedyourfin.in added to cors

--- src/utils/corsOrgin.ts
+++ src/utils/corsOrgin.ts
@@ -3,7 +3,7 @@
 
 const corsOrigin =
   process.env.NODE_ENV === 'prod'
-    ? ['https://speedyourfin.ai', 'https://www.speedyourfin.ai' , 'https://syf.speedyourfin.ai','http://localhost:3080', 'http://desktop-dssus3g:82']
+    ? ['https://speedyourfin.ai', 'https://www.speedyourfin.ai' , 'https://syf.speedyourfin.ai','http://localhost:3080', 'http://desktop-dssus3g:82', 'https://syftestenv.speedyourfin.in']
     : ['http://localhost:3080', 'http://desktop-dssus3g:82']
 
 const cookieDomain =
